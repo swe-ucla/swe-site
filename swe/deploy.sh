db89c3afb6fe56672a6e2e1b47ea4e86daf30c6a
christinatong01 <64873950+christinatong01@users.noreply.github.com>
2021-01-07 15:24:02 -0800

    Create deploy.sh

---
+++ swe/deploy.sh
@@ -0,0 +1,8 @@
+THEDATE=`date`
+npm run-script build
+cd build
+cp -a . ../../swe-ucla.github.io/
+cd ../../swe-ucla.github.io/
+git add -A
+git commit -m "Site updated at $THEDATE"
+git push origin master
