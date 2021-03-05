73133f0e059a7b6f2facaf6ea1ef603ee7b372a0
Christina Tong <christinatong01@g.ucla.edu>
2021-03-04 16:36:39 -0800

    errors

--- swe/src/App.js
+++ swe/src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import Main from "./components/main";
-<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";
-=======
-import { BrowserRouter} from "react-router-dom";
->>>>>>> 136c8b7d9b6c41313fb4ecdece2b9b97b79726ff
 
 function App() {
   return (
