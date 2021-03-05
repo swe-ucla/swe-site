40f0ebe6553a572045b8d69011b64555a62d4a91
Christina Tong <christinatong01@g.ucla.edu>
2021-03-04 16:55:30 -0800

    nav bar issues

--- swe/src/App.js
+++ swe/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Main from "./components/main";
 import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/swe-site">
+      <BrowserRouter basename="/swe-site"> </BrowserRouter>
+
+      <HashRouter>
         <Main />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
