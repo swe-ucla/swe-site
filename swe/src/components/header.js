c643ed0ae20efe2ff945f3eb5ffb1073a17f771c
Megan Go <Meganggo11@gmail.com>
2021-11-23 20:29:31 -0800

    menu button
    - menu button that links to different pages is visible when the window size is small

--- swe/src/components/header.js
+++ swe/src/components/header.js
@@ -1,5 +1,7 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/layout.css";
+import Dropdown from 'react-bootstrap/Dropdown'
 
 import logo from "../images/swe-ucla-logo.png";
 
@@ -8,7 +10,8 @@ export default class header extends React.Component {
         super(props);
         this.state = {
             windowSize: "",
-            buttonClass: "button"
+            buttonClass: "visible-button",
+            dropDownClass: "dropdown-invisible"
         };
     }  
 
@@ -16,19 +19,23 @@ export default class header extends React.Component {
         const windowSize = window.innerWidth;
         if(windowSize <= 840){
             const buttonClass = "invisible-button";
+            const dropDownClass = "dropdown-visible";
             this.setState(prevState =>{
                 return{
                     windowSize,
-                    buttonClass
+                    buttonClass,
+                    dropDownClass
                 };
             });
         }
         else{
-            const buttonClass = "button";
+            const buttonClass = "visible-button";
+            const dropDownClass = "dropdown-invisible";
             this.setState(prevState =>{
                 return{
                     windowSize,
-                    buttonClass
+                    buttonClass,
+                    dropDownClass
                 };
             });
         }
@@ -45,14 +52,25 @@ export default class header extends React.Component {
         window.location.href = "#portal";
     }
 
+
+
     render() {
       return (
-          <div className="header">
+           <div className="header">
               <a href = "#"><img src={logo} style={{height:55, width: 90, position:"absolute", left:50, top:15}}/></a>
-              <a href="#about-us" class={this.state.buttonClass}>About Us</a>
-              <a href="#involved" class={this.state.buttonClass}>Get Involved</a>
-              <a href="#events" class={this.state.buttonClass}>Events</a>
-              <a href="#sponsors" class={this.state.buttonClass}>Sponsors</a>
+              <a href="#about-us" className={this.state.buttonClass}>About Us</a>
+              <a href="#involved" className={this.state.buttonClass}>Get Involved</a>
+              <a href="#events" className={this.state.buttonClass}>Events</a>
+              <a href="#sponsors" className={this.state.buttonClass}>Sponsors</a>
+              <Dropdown>
+                 <Dropdown.Toggle variant="success" className={this.state.dropDownClass} id="dropdown-basic" style={{position: "absolute", top: 20, right: 2}}>Menu</Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#about-us">About Us</Dropdown.Item>
+                    <Dropdown.Item href="involved">Get Involved</Dropdown.Item>
+                    <Dropdown.Item href="#events">Events</Dropdown.Item>
+                    <Dropdown.Item href="#sponsors">Sponsors</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
               <button className="form_button" onClick={() => {this.memberHandler()}}>Member Portal</button>
           </div>
       )
