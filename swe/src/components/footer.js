927c5d1de33fa4d910463f960b33cc329a22c799
mop03 <36209272+mop03@users.noreply.github.com>
2021-12-23 20:05:52 -0800

    footer
    got rid of mainly flex:1

--- swe/src/components/footer.js
+++ swe/src/components/footer.js
@@ -22,8 +22,8 @@ export default class footer1 extends React.Component {
         <div className="footer1">
         {/* <img src={footer} className="committee_event_pics" style={{position:"absolute", left:0, width:1440, top:2050, bottom:0}}/>
         <img src={dot} style={{position:"absolute", left:120, top:2100, width:2, height:200}}/> */}
-         <img src={footer} className="committee_event_pics" />
-        <img src={dot} />  
+   {/*    <img src={footer} className="committee_event_pics" /> */}
+        <img src={dot} style={{width:2, height:140}}/>  
 
             <div className="footer15"> 
                 <a href="#about-us" onClick={() => {this.tempHandler()}} class="foot-left">About Us</a> 
@@ -39,7 +39,8 @@ export default class footer1 extends React.Component {
             </div>
 
             <div className="rightdm">
-                <a href="#involved" class="foot-button">Email us at swe@seas.ucla.edu</a>
+               <a href="#involved" class="foot-button">Email us at swe@seas.ucla.edu</a> 
+          
                 <a href="#involved" class="foot-button">DM us on Facebook for quick inquiries</a> 
             </div>
 
